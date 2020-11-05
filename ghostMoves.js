1bcd949ee0e3ab094c41251539a609119678f181
willis240 <willingness240@gmail.com>
2020-11-04 18:51:05 -0900

    added code for basic pacman, ghost AI needs improving

---
+++ ghostMoves.js
@@ -0,0 +1,23 @@
+import {DIRECTIONS, OBJECT_TYPE} from './setup'
+
+// Random Movement (Just for use as a prototype)
+export function randomMovement(postion, direction, objectExist)
+{
+    let dir = direction;
+    let nextMovePos = position + dir.movement;
+    // Create an array from the directions object leys
+    const keys = Object.keys(DIRECTIONS);
+
+    while (
+        objectExist(nextMovePos, OBJECT_TYPE.WALL) ||
+        objectExist(nextMovePos, OBJECT_TYPE.GHOST)
+    ){
+        // Get a random key from the key array
+        const key = keys[Math.floor(Math.random() * keys.length)];
+        // Set the next move
+        dir = DIRECTIONS[key];
+        // Set the next move
+        nextMovePos = position + dir.movement;
+    }
+    return {nextMovePos, direction: dir};
+}
